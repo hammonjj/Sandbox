1f9745a68e7c94a7b4a95b66d47174c64359d5de
hammonjj <james.hammond@thegruppetto.com>
2023-01-10 17:17:38 -0700

    Started updating debugging HUD

--- Unity/ProjectConstantine/Assets/Scripts/DebugHud.cs
+++ Unity/ProjectConstantine/Assets/Scripts/DebugHud.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DebugHud : MonoBehaviourBase
 {
     //Scene Name
-    //
-
+    //Door List
+    //  - Next Scene
+    //  - Room Reward
+    private DoorManager _doorManager;
+    private GameStateManager _gameStateManager;
+    private SceneStateManager _sceneStateManager;
+    
     private void Start()
     {
         
@@ -14,11 +20,53 @@ public class DebugHud : MonoBehaviourBase
 
     private void Update()
     {
-        
+        if(_doorManager == null)
+        {
+            _doorManager = GameObject.FindGameObjectWithTag(Constants.DoorManager)?.GetComponent<DoorManager>();
+            if(_doorManager == null)
+            {
+                return;
+            }
+
+            LogDebug("Acquired Door Manager");
+        }
+
+        if(_sceneStateManager == null)
+        {
+            _sceneStateManager = GameObject.FindGameObjectWithTag(Constants.SceneStateManager)?.GetComponent<SceneStateManager>();
+            if(_sceneStateManager == null)
+            {
+                return;
+            }
+
+            LogDebug("Acquired Scene Manager");
+        }
+
+        if(_gameStateManager == null)
+        {
+            _gameStateManager = GameObject.FindGameObjectWithTag(Constants.GameStateManager)?.GetComponent<GameStateManager>();
+            if(_gameStateManager == null)
+            {
+                return;
+            }
+
+            LogDebug("Acquired Game Manager");
+        }
     }
 
     private void OnGUI()
     {
-        GUI.Label(new Rect(10, 10, 100, 20), "Hello World!");
+        if(_doorManager == null ||
+            _sceneStateManager == null ||
+            _gameStateManager == null)
+        {
+            LogDebug("One or more managers are null - waiting");
+            return;
+        }
+
+        //LogDebug($"SceneManager.GetActiveScene().name: {SceneManager.GetActiveScene().name}");
+        var currentScene = "Scene: " + SceneManager.GetActiveScene().name;
+        LogDebug(currentScene);
+        GUI.Label(new Rect(10, 10, 100, 20), currentScene);
     }
 }
