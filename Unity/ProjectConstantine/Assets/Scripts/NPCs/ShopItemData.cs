63762cf7ab834651114e6fcb32222027bb775050
hammonjj <james.hammond@thegruppetto.com>
2023-02-02 15:59:54 -0700

    Started adding upgrade items

---
+++ Unity/ProjectConstantine/Assets/Scripts/NPCs/ShopItemData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ShopItemData", menuName = "ShopItems/ShopItemData")]
+public class ShopItemData : ScriptableObjectBase
+{
+    public string Name;
+    public string Description;
+    public int Cost;
+}
---
+++ Unity/ProjectConstantine/Assets/Scripts/NPCs/ShopItemUiTemplate.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using TMPro;
+
+public class ShopItemUiTemplate : MonoBehaviour
+{
+    public TextMeshProUGUI Title;
+    public TextMeshProUGUI Description;
+    public TextMeshProUGUI Cost;
+}
