92fbcdf4e9b1f75399a88dcf6bec6207c6a6f363
hammonjj <james.hammond@thegruppetto.com>
2023-02-03 09:03:58 -0700

    Updated shop manager

--- Unity/ProjectConstantine/Assets/Scripts/NPCs/ShopManager.cs
+++ Unity/ProjectConstantine/Assets/Scripts/NPCs/ShopManager.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class ShopManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ShopItemData[] ShopItemData;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
