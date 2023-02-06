fec0217c14e77536df79f1c6a0d09debf46f6a75
hammonjj <james.hammond@thegruppetto.com>
2023-02-06 14:16:09 -0700

    Updated debugging tools window

--- Unity/ProjectConstantine/Assets/Scripts/Framework/EditorExtensions/DebugGameWindow.cs
+++ Unity/ProjectConstantine/Assets/Scripts/Framework/EditorExtensions/DebugGameWindow.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
@@ -46,6 +47,38 @@ public class DebugGameWindow : EditorWindow
             Helper.LogDebug("Load Zone 1 Start");
             EditorSceneManager.OpenScene("Assets/Scenes/ZoneOne/" +
                 Constants.Enums.Scenes.Zone1_Start.ToString() + ".unity");
+        }
+
+        if(GUI.Button(new Rect(0, 150, 200, 25), "Load Scene"))
+        {
+            var menu = new GenericMenu();
+            foreach(int scene in Enum.GetValues(typeof(Constants.Enums.Scenes)))
+            {
+                var sceneName = ((Constants.Enums.Scenes)scene).ToString();
+                AddSceneToMenu(menu, sceneName, sceneName);
+            }
+
+            menu.ShowAsContext();
+        }
+    }
+
+    private void AddSceneToMenu(GenericMenu menu, string menuPath, string sceneName)
+    {
+        menu.AddItem(new GUIContent(menuPath), true, () => OnSceneSelected(sceneName));
+    }
+
+    private void OnSceneSelected(string sceneName)
+    {
+        if(SceneManager.GetActiveScene().isDirty)
+        {
+            if(EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            {
+                EditorSceneManager.OpenScene($"Assets/Scenes/ZoneOne/{sceneName}.unity");
+            }
+        }
+        else
+        {
+            EditorSceneManager.OpenScene($"Assets/Scenes/ZoneOne/{sceneName}.unity");
         }
     }
 }
