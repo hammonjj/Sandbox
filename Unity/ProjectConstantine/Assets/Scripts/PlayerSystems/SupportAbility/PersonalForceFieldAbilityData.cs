4fde785bbe472a91f7cb45388678ab0cb26a1784
hammonjj <james.hammond@thegruppetto.com>
2023-02-07 16:57:18 -0700

    Update PersonalForceFieldAbilityData.cs

--- Unity/ProjectConstantine/Assets/Scripts/PlayerSystems/SupportAbility/PersonalForceFieldAbilityData.cs
+++ Unity/ProjectConstantine/Assets/Scripts/PlayerSystems/SupportAbility/PersonalForceFieldAbilityData.cs
@@ -5,16 +5,11 @@ public class PersonalForceFieldAbilityData : SupportAbilityBaseData
 {
     public GameObject ForceFieldPrefab;
 
-    private GameObject _player;
-
-    private void Awake()
-    {
-        _player = GameObject.FindGameObjectWithTag(Constants.Tags.Player);    
-    }
-
     public override void OnUse()
     {
         LogDebug("Initializing Forcefield");
-        Instantiate(ForceFieldPrefab, _player.transform);
+
+        var player = GameObject.FindGameObjectWithTag(Constants.Tags.Player);
+        Instantiate(ForceFieldPrefab, player.transform);
     }
 }
